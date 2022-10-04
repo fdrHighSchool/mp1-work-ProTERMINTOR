abd8df59616dd845687350d39b224ac2ab0f3091
Ap Java Period 5 <apjavaperiod5@ac1mwk0f6jc9h.78b505.nycboe.org>
2022-10-04 12:07:32 -0400

    added files favrotie movie and rocket ship

---
+++ 01-favorite-movie/Favoritemovie.java
@@ -0,0 +1,14 @@
+
+/**
+ * Write a description of class Favoritemovie here.
+ *
+ * @author 
+ * @version (a version number or a date)
+ */
+public class Favoritemovie
+{
+    public static void main(String [] args) {
+        System.out.println("my favorite movie is star wars");
+        System.out.println("the prequals and orignals to be specfic");
+    }
+}
---
+++ 02-rocketship/Rocket.java
@@ -0,0 +1,29 @@
+
+/**
+ * Write a description of class Rocket here.
+ *
+ * @author Michael Arocho and 
+ * @version (a version number or a date)
+ */
+public class Rocket 
+{ 
+ public static void main(String[]args) {
+    triangle();
+    square();
+    System.out.println("|United|" + "\n" + "|States|");
+    square();
+    triangle();
+ }//End of method main
+ public static void triangle() {
+    System.out.println("   /\\ ");
+    System.out.println("  /  \\");
+    System.out.println(" /    \\");
+   } 
+ public static void square() {
+     System.out.println("+------+");
+     System.out.println("|      |");
+     System.out.println("|      |");
+     System.out.println("+------+");
+    }
+ }
+
