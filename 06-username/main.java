97c634400cf343d1f29442499bf4066ff7c3f96a
Ap Java Period 5 <apjavaperiod5@ac1mwk0f6jc9h.78b505.nycboe.org>
2022-10-12 12:14:35 -0400

    added username

---
+++ 06-username/main.java
@@ -0,0 +1,12 @@
+
+/**
+ * Write a description of class username here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+import java.util.Scanner;
+public class main
+{
+   
+}
